9dc46ef81a53c7db935cffa59d8605e99ce3f076
vasanth <s.vasanthrojin@gmail.com>
2021-12-19 14:18:46 +0800

    mods to structure

--- webapp/main.py
+++ webapp/main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET", "POST"])
 def hello_world():
-    if len(request.form) > 0:
-        pass
-    else:
-        des_count=1
-    return render_template("index.html",des_count=des_count,source_address="the nano address :3",source_amount=69,Destinations=[{"destination_address":"address 1","destination_amount":60},{"destination_address":"address 2","destination_amount":9}])
+    des_count = 1
+    if request.form is not None:
+        print(request.form)
+        
+    return render_template("index.html",des_count=des_count,source_address="the nano address",source_amount=69,Destinations=[{"destination_address":"address 1","destination_amount":60},{"destination_address":"address 2","destination_amount":9}])
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
